e755c25b4da8ca63361cd9a57fda7a7dd40e20fb
KingJrn <emmajnr503@protonmail.com>
2025-04-12 11:03:18 +0100

    Completed

--- ValuationAfrica/src/views/Login.vue
+++ ValuationAfrica/src/views/Login.vue
@@ -22,7 +22,6 @@
             <input
               id="email-address"
               name="email"
-              type="email"
               required
               v-model="email"
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
